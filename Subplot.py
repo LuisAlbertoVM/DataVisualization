8411482f3f5b4ae7c9710e17cecaa61978821687
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-02 19:41:12 -0500

    the order of the plots

--- Subplot.py
+++ Subplot.py
@@ -5,7 +5,21 @@ x = np.linspace(0,5,11)
 y = x**2
 
 plt.subplot(1,2,1)
-plt.plot(x,y)
+plt.plot(x,y, 'r--')
 plt.subplot(1,2,2)
 plt.hist(y)
+plt.show()
+
+plt.subplot(1,2,1)
+plt.plot(x,y, 'r--')
+plt.plot(y,x, 'b:')
+plt.subplot(1,2,2)
+plt.pie(y)
+plt.show()
+
+plt.subplot(2,1,1)
+plt.plot(x,y, 'r--')
+plt.plot(y,x, 'b:')
+plt.subplot(2,1,2)
+plt.pie(y)
 plt.show()
