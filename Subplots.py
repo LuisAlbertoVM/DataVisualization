a8d89e716e13ddba9a20614c2ba03986409a2e38
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-03 20:30:28 -0500

    Subplots with Object

--- Subplots.py
+++ Subplots.py
@@ -3,3 +3,10 @@ import numpy as np
 
 x = np.linspace(0,5,11)
 y = np.sin(x)
+
+fig, axes = plt.subplots()
+axes.plot(x,y,'b')
+plt.show()
+
+fig, axes = plt.subplots(nrows = 1, ncols=2)
+#axes.plot(x,y,'b')
