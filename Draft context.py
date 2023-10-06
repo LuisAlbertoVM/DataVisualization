649fdc2f90bae20012a8a5038f01c63c115039f7
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-05 21:49:16 -0500

    example with object oriented method and without object

--- Draft context.py
+++ Draft context.py
@@ -4,9 +4,21 @@ import numpy as np
 x = np.linspace(0,5,11)
 y = np.sin(x)
 
-fig, axes = plt.subplots(1,2)
-axes[0].plot(x,y)
+fig, axes = plt.subplots(1,2,figsize=(5,5))
+axes[0].plot(x,y,label='$sin(x)$')
 axes[0].set_title('Relation X-Y')
+axes[0].set_xlabel('X')
+axes[0].set_ylabel('Y')
+axes[0].legend()
 axes[1].plot(y,x)
 axes[1].set_title('Relation Y-X')
+axes[1].set_xlabel('Y')
+axes[1].set_ylabel('X')
+plt.show()
+
+# without object methods
+plt.plot(x,y,label='$sin(x)$')
+plt.title('This is the title')
+plt.xlabel('x')
+plt.ylabel('y')
 plt.show()
