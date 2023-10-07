ff6d4ae7586074ca2f0e05990f443ec2cc6f36d6
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-06 23:15:21 -0500

    add stat, multiple

--- Distributions.py
+++ Distributions.py
@@ -3,3 +3,6 @@ import matplotlib.pyplot as plt
 
 tips = sns.load_dataset('tips')
 print(tips)
+
+sns.histplot(tips, x='tip', bins = 15, cumulative=False, hue='sex', stat='count', multiple='dodge')
+plt.show()
