02f8ea511e271e71390db27af1a8c9bba80a1b60
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-07 10:28:21 -0500

    select the markers

--- Relation.py
+++ Relation.py
@@ -4,7 +4,13 @@ import matplotlib.pyplot as plt
 tips = sns.load_dataset("tips")
 print(tips.head(2))
 
+plt.figure(figsize=(8,6))
 sns.scatterplot(data=tips,x='total_bill',y='tip', hue='day',style='time',size='size')
-plt.legend(loc='center',bbox_to_achor=(1.12,0,5))
+plt.legend(loc='center',bbox_to_anchor=(1.12,0.5))
 plt.show()
 
+plt.figure(figsize=(8,6))
+markers = {"Lunch":"D","Dinner":"s"}
+sns.scatterplot(data=tips,x='total_bill',y='tip', hue='day',style='time',size='size',markers=markers)
+plt.legend(loc='center',bbox_to_anchor=(1.12,0.5))
+plt.show()
