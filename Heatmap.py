c25c7a861c8dc43a760531ad397ae9fb2f3dc775
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-07 11:03:39 -0500

    use of heatmap

--- Heatmap.py
+++ Heatmap.py
@@ -5,3 +5,6 @@ tips = sns.load_dataset('tips')
 print(tips.head(2))
 
 print(tips.drop(columns=['sex','smoker','day','time']).corr())
+
+sns.heatmap(tips.drop(columns=['sex','smoker','day','time']).corr())
+plt.show()
