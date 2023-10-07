29b7e128b68f119178c6fb1483a31364ac00362e
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-07 10:44:44 -0500

    use of jointplot

--- Joinplot-Pairplot.py
+++ Joinplot-Pairplot.py
@@ -4,5 +4,5 @@ import matplotlib.pyplot as plt
 tips = sns.load_dataset("tips")
 print(tips.head(2))
 
-sns.jointplot(data=tips,x='total_bill',y='tip')
+sns.jointplot(data=tips,x='total_bill',y='tip',hue='sex',kind='hist',marginal_ticks=True,marginal_kws=dict(bins=25,fill=False,multiple='dodge'))
 plt.show()
