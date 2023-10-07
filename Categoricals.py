6a0c465e76e441963a7191a5b96103b9c9bacef5
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-07 10:10:39 -0500

    use of split

--- Categoricals.py
+++ Categoricals.py
@@ -25,5 +25,5 @@ sns.swarmplot(data=tips,x='day',y='total_bill',hue='sex',dodge=True, color='0',m
 plt.show()
 
 plt.figure(figsize=(6,6))
-sns.violinplot(data=tips,x='day',y='total_bill',hue='sex',palette='pastel')
+sns.violinplot(data=tips,x='day',y='total_bill',hue='sex',palette='pastel',split=True)
 plt.show()
