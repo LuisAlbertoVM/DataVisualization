8c5a4554ad3b4251608e3574720ecec11692b301
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-06 22:54:26 -0500

    default ways to show the data

--- parameters.py
+++ parameters.py
@@ -3,3 +3,12 @@ import matplotlib.pyplot as plt
 
 tips = sns.load_dataset('tips')
 print(tips)
+
+sns.displot(data=tips, x= 'total_bill')
+plt.show()
+
+sns.displot(data=tips, x= 'total_bill', y='tip')
+plt.show()
+
+sns.displot(data=tips, x= 'total_bill', y='tip', hue='sex')
+plt.show()
