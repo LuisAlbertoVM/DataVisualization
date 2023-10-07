800da1780e37ce62cc07707fba850b8d4eb5b1d3
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-06 21:37:19 -0500

    Add file Bar Plot.py

---
+++ Bar Plot.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+country = ['India', 'Japan', 'Mexico', 'Colombia', 'Germany']
+population = [100, 800, 900, 1000, 300]
+
+plt.bar(country, population)
+plt.show()
