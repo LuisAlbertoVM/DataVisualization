7d6aecaabc17bd7ec541f91285aee48014813183
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-06 21:43:11 -0500

    Added Width, Color and Horizontal bars

--- Bar Plot.py
+++ Bar Plot.py
@@ -2,7 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 country = ['India', 'Japan', 'Mexico', 'Colombia', 'Germany']
-population = [100, 800, 900, 1000, 300]
+population = [1000, 800, 900, 1000, 300]
 
-plt.bar(country, population)
+plt.bar(country, population, width=0.5, color = ['red', 'blue'])
+plt.xticks(np.arange(5),('India', 'Japon', 'Mexico', 'Colombia', 'Alemania'),rotation = 45)
+plt.show()
+
+plt.barh(country, population)
 plt.show()
