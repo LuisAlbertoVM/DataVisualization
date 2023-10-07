1775c7d7eb6d6a231897477907eb5ab4862d0820
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-07 10:25:06 -0500

    use of scatterplot

--- Relation.py
+++ Relation.py
@@ -4,5 +4,7 @@ import matplotlib.pyplot as plt
 tips = sns.load_dataset("tips")
 print(tips.head(2))
 
-sns.scatterplot(data=tips,x='total_bill',y='tip')
+sns.scatterplot(data=tips,x='total_bill',y='tip', hue='day',style='time',size='size')
+plt.legend(loc='center',bbox_to_achor=(1.12,0,5))
 plt.show()
+
