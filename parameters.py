176293cb4b1ffbb4d6d4a90aa7013f5908c3eef9
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-06 22:57:12 -0500

    added kind, legend

--- parameters.py
+++ parameters.py
@@ -12,3 +12,9 @@ plt.show()
 
 sns.displot(data=tips, x= 'total_bill', y='tip', hue='sex')
 plt.show()
+
+sns.displot(data=tips, x= 'total_bill', y='tip', hue='sex', kind='kde')
+plt.show()
+
+sns.displot(data=tips, x= 'total_bill', hue='sex', kind='kde', legend=True)
+plt.show()
