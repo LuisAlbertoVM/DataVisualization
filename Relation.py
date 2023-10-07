415e1a967b35602076e53aa41925252c37cc2be0
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-07 10:28:50 -0500

    configure the figsize

--- Relation.py
+++ Relation.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 tips = sns.load_dataset("tips")
 print(tips.head(2))
 
-plt.figure(figsize=(8,6))
+plt.figure(figsize=(8,8))
 sns.scatterplot(data=tips,x='total_bill',y='tip', hue='day',style='time',size='size')
 plt.legend(loc='center',bbox_to_anchor=(1.12,0.5))
 plt.show()
 
-plt.figure(figsize=(8,6))
+plt.figure(figsize=(8,8))
 markers = {"Lunch":"D","Dinner":"s"}
 sns.scatterplot(data=tips,x='total_bill',y='tip', hue='day',style='time',size='size',markers=markers)
 plt.legend(loc='center',bbox_to_anchor=(1.12,0.5))
