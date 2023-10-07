33247d0bc6f0c746e910a509d28882e23b20e850
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-07 09:54:25 -0500

    use of swarmplot

--- Categoricals.py
+++ Categoricals.py
@@ -7,5 +7,10 @@ print(tips.head(2))
 sns.countplot(data=tips,y='day',hue='sex')
 plt.show()
 
+plt.figure(figsize=(6,6))
 sns.stripplot(data=tips,x='day',y='total_bill',hue='sex',dodge=True)
+plt.show()
+
+plt.figure(figsize=(6,6))
+sns.swarmplot(data=tips,x='day',y='total_bill',hue='sex',dodge=True)
 plt.show()
