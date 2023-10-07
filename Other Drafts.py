14fea4f3992523c2c65aaab9e3011d0b54d7fbd1
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-06 22:09:50 -0500

    added Boxplot, scatter

--- Other Drafts.py
+++ Other Drafts.py
@@ -7,5 +7,16 @@ print('Histogram')
 plt.hist(data, bins=10,histtype='bar')
 plt.show()
 
-plt.hist(data, bins=10,histtype='step')
+print('Boxplot')
+data = np.append(data,200)
+plt.boxplot(data, vert= False, patch_artist = True,notch=True, showfliers = True)
+plt.show()
+
+N = 50
+x = np.random.rand(N)
+y = np.random.rand(N)
+area = (30 * np.random.rand(N)) ** 2
+colors = np.random.rand(N)
+
+plt.scatter(x,y, s=area, c = colors, marker='o', alpha=0.5)
 plt.show()
