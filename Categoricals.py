9d6f92dccc180e60d8cc65f57c6485359df7ff66
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-07 09:51:01 -0500

    use of stripplot

--- Categoricals.py
+++ Categoricals.py
@@ -4,5 +4,8 @@ import matplotlib.pyplot as plt
 tips = sns.load_dataset("tips")
 print(tips.head(2))
 
-sns.countplot(data=tips,x='day',hue='sex')
+sns.countplot(data=tips,y='day',hue='sex')
+plt.show()
+
+sns.stripplot(data=tips,x='day',y='total_bill',hue='sex',dodge=True)
 plt.show()
