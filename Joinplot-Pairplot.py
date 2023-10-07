aca7cc6e820eb1da26b9eaf89b9684357b3966bb
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-07 10:48:34 -0500

    use of pairplot

--- Joinplot-Pairplot.py
+++ Joinplot-Pairplot.py
@@ -7,5 +7,5 @@ print(tips.head(2))
 sns.jointplot(data=tips,x='total_bill',y='tip',hue='sex',kind='hist',marginal_ticks=True,marginal_kws=dict(bins=25,fill=False,multiple='dodge'))
 plt.show()
 
-sns.pairplot(data=tips, hue='sex',palette='coolwarm')
+sns.pairplot(data=tips, hue='sex',palette='coolwarm',corner=True)
 plt.show()
