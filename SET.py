9968204f8eaaa469e3c17abd635ce1bd991c7494
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-06 22:46:43 -0500

    add style, palette, font, font_scale

--- SET.py
+++ SET.py
@@ -1,1 +1,9 @@
 import seaborn as sns
+import matplotlib.pyplot as plt
+
+sns.barplot(x=['A','B','C'], y=[1,3,2])
+plt.show()
+
+sns.set(style='darkgrid',palette='muted', font='Verdana', font_scale=1)
+sns.barplot(x=['A','B','C'], y=[1,3,2])
+plt.show()
