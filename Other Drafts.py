933fd59d6631a12e2e2817e2e777098b6f875ea0
LuisAlbertoVM <luis.alberto.v.m@hotmail.com>
2023-10-06 21:55:05 -0500

    add histtype

--- Other Drafts.py
+++ Other Drafts.py
@@ -4,5 +4,8 @@ import numpy as np
 data = np.random.randint(1, 50, 100)
 
 print('Histogram')
-plt.hist(data)
+plt.hist(data, bins=10,histtype='bar')
+plt.show()
+
+plt.hist(data, bins=10,histtype='step')
 plt.show()
